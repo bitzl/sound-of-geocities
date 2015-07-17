cbb7ce9912f5b3c68f98a69bc4883b17fb8fac40
Marcus Bitzl <marcus@bitzl.com>
2015-07-18 00:03:13 +0200

    Added missing class.

---
+++ src/main/java/com/bitzl/soundofgeocities/MidiEventCounter.java
@@ -0,0 +1,18 @@
+package com.bitzl.soundofgeocities;
+
+
+import com.bitzl.soundofgeocities.model.EventCounts;
+
+import javax.sound.midi.MidiEvent;
+
+public class MidiEventCounter {
+
+    public void count(MidiEvent event) {
+
+    }
+
+    public EventCounts getEventCounts() {
+        return null;
+    }
+
+}
